79ed777938b5d2022ae24c6cc97ff4a7707586f1
Nimesh Perera <mattperdevstuff@gmail.com>
2022-05-17 07:06:54 +0530

    vercel deploy error#1

--- airbnb-clone/components/Footer.js
+++ airbnb-clone/components/Footer.js
@@ -1,6 +1,7 @@
 
 function Footer() {
   return (
+    <div>
     <div className="grid grid-cols-1 md:grid-cols-2 space-y-10 px-32 py-14 bg-gray-100 text-gray-600">
         <div className="space-y-4 text-xs text-gray-800">
             <h5 className="font-bold">ABOUT</h5>
@@ -17,6 +18,12 @@ function Footer() {
             <p>Trust & safety</p>
         </div>
         
+    </div>
+
+    <div className="bg-gray-800 border-2 py-2">
+      <p className="flex items-center space-y-4 text-xs text-gray-400">Created by Mattper</p>
+    </div>
+      
     </div>
   )
 }
